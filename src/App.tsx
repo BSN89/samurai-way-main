cdc7118c5f9aa912bcf96ab6cb1eef2efeb6b75a
BSN89 <barakovsn55@gmail.com>
2023-02-21 07:24:34 +0300

    change README.md

--- src/App.tsx
+++ src/App.tsx
@@ -4,50 +4,79 @@ import './App.css';
 
 
 function App() {
+    console.log("App rendering")
     return (
         <div>
-            This is App component
+            <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
             <Star/>
         </div>
-    );
+)
+}
+
+function AppTitle() {
+    console.log("AppTitle rendering")
+    return (
+        <>This is App component</>
+    )
 }
 
 function Rating() {
+    console.log("Rating rendering")
     return (
         <div>
-            <div>start</div>
-            <div>start</div>
-            <div>start</div>
-            <div>start</div>
-            <div>start</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
         </div>
     )
 }
 
 function Accordion() {
+    console.log("Accordion rendering")
+    return (
+        <>
+        <AccordionTitle/>
+        <AccordionBody/>
+        </>
+)
+}
+
+function Star() {
+    console.log("Star rendering")
     return (
         <div>
-            <h3>Меню</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <div>Star</div>
+            <div>Star</div>
+            <div>Star</div>
+            <div>Star</div>
+            <div>Star</div>
         </div>
     )
 }
 
-function Star() {
+function AccordionTitle() {
+    console.log("AccordionTitle rendering")
     return (
-        <div>
-            <div>Star</div>
-            <div>Star</div>
-            <div>Star</div>
-            <div>Star</div>
-            <div>Star</div>
-        </div>
+        <>
+            <h3>Меню</h3>
+        </>
+    )
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendering")
+    return (
+        <> <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+        </>
     )
 }
 
