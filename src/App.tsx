cb0b94820c2886142f3dabb1807627d7aa2394a5
BSN89 <barakovsn55@gmail.com>
2023-02-21 06:34:05 +0300

    change README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            This is App component
+            <Rating/>
+            <Accordion/>
+            <Star/>
+        </div>
+    );
 }
+
+function Rating() {
+    return (
+        <div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+        </div>
+    )
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Меню</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+
+function Star() {
+    return (
+        <div>
+            <div>Star</div>
+            <div>Star</div>
+            <div>Star</div>
+            <div>Star</div>
+            <div>Star</div>
+        </div>
+    )
+}
+
+
 
 export default App;
