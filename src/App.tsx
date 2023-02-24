c522966ad64958c50e81dcd7400a987c46b34653
BSN89 <barakovsn55@gmail.com>
2023-02-24 12:51:21 +0300

    change README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Accordion from "./Accordion/Accordion";
-
+import Accordion from "./components/Accordion/Accordion";
+import {Rating, Star} from "./components/Rating/Rating";
 
 
 function App() {
@@ -21,34 +21,6 @@ function AppTitle() {
     console.log("AppTitle rendering")
     return (
         <>This is App component</>
-    )
-}
-
-function Rating() {
-    console.log("Rating rendering")
-    return (
-        <div>
-            <div>star</div>
-            <div>star</div>
-            <div>star</div>
-            <div>star</div>
-            <div>star</div>
-        </div>
-    )
-}
-
-
-
-function Star() {
-    console.log("Star rendering")
-    return (
-        <div>
-            <div>Star</div>
-            <div>Star</div>
-            <div>Star</div>
-            <div>Star</div>
-            <div>Star</div>
-        </div>
     )
 }
 
---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export function Rating() {
+    console.log("Rating rendering")
+    return (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+    )
+}
+
+export function Star() {
+    console.log("Star rendering")
+    return (
+        <div>
+            <div>Star</div>
+            <div>Star</div>
+            <div>Star</div>
+            <div>Star</div>
+            <div>Star</div>
+        </div>
+    )
+}
